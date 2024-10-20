9ad74f8d079b79947e98e1c242618a07930025ee
SurajIngole <suraj.ingole7024@gmail.com>
2024-10-20 18:41:07 +0530

    Used advanced routing, regex, query params, dynamic params

--- src/app.js
+++ src/app.js
@@ -2,8 +2,22 @@ const express= require('express');
 
 const app = express();
 
+
+app.get(/.*fly$/,(req,res)=>{   
+    res.send({message:'Used regex'
+    })
+});
+app.get('/user/:userId/:name/:pass',(req,res)=>{
+    console.log(req.params);
+    res.send({message:'dynamic params'})
+});
+app.get('/user/:userId',(req,res)=>{
+    console.log(req.params);    
+    res.send({message:'Used dynamic params'})
+});
 app.get('/user',(req,res)=>{
-    res.send({firstname: 'Suraj', lastname: 'Ingole'})
+    console.log(req.query);
+    res.send({message:'Used query params'})
 });
 
 app.post('/user',(req,res)=>{
