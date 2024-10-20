715290f3059a03df38e849f854491b3172040668
SurajIngole <suraj.ingole7024@gmail.com>
2024-10-20 18:16:22 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -2,13 +2,20 @@ const express= require('express');
 
 const app = express();
 
+app.get('/user',(req,res)=>{
+    res.send({firstname: 'Suraj', lastname: 'Ingole'})
+});
+
+app.post('/user',(req,res)=>{
+    res.send({message: 'User created successfully',errorMessage:null});
+});
+
+app.delete('/user',(req,res)=>{
+    res.send({message: 'User deleted successfully',errorMessage:null});
+});
 app.use("/test",(req,res)=>{
     res.send("Hello, Test!");
 });
-app.use("/",(req,res)=>{
-    res.send("Hello!");
-});
-
 
 app.listen(7777,() => {
     console.log('Server is running on port 7777');
