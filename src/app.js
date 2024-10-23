605e61ce2581ae483d46737835c4d0c7a7d25a6f
SurajIngole <suraj.ingole7024@gmail.com>
2024-10-23 15:31:48 +0530

    used multiple route handler

--- src/app.js
+++ src/app.js
@@ -3,33 +3,26 @@ const express= require('express');
 const app = express();
 
 
-app.get(/.*fly$/,(req,res)=>{   
-    res.send({message:'Used regex'
-    })
-});
-app.get('/user/:userId/:name/:pass',(req,res)=>{
-    console.log(req.params);
-    res.send({message:'dynamic params'})
-});
-app.get('/user/:userId',(req,res)=>{
-    console.log(req.params);    
-    res.send({message:'Used dynamic params'})
-});
-app.get('/user',(req,res)=>{
-    console.log(req.query);
-    res.send({message:'Used query params'})
-});
-
-app.post('/user',(req,res)=>{
-    res.send({message: 'User created successfully',errorMessage:null});
-});
-
-app.delete('/user',(req,res)=>{
-    res.send({message: 'User deleted successfully',errorMessage:null});
-});
-app.use("/test",(req,res)=>{
-    res.send("Hello, Test!");
-});
+app.use('/user',
+    (req,res,next)=>{
+        console.log('User middleware 1');
+        next();
+    },
+    (req,res,next)=>{
+        console.log('User middleware 2');
+        //res.send('request handler 2')
+        next();
+    },
+    (req,res,next)=>{
+        console.log('User middleware 3');
+        //res.send('request handler 3')
+        next();
+    },
+    (req,res,next)=>{
+        console.log('User middleware 4');
+       res.send('request handler 4')
+    }
+);
 
 app.listen(7777,() => {
     console.log('Server is running on port 7777');
