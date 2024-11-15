b815425d7b5e7e4bccee90136ae64a49964b4f3b
SurajIngole <suraj.ingole7024@gmail.com>
2024-11-15 19:28:38 +0530

    Added data sanitization and data validation.

--- src/app.js
+++ src/app.js
@@ -1,92 +1,108 @@
-const express= require('express');
-const {connectDB}= require('./config/database');
+const express = require("express");
+const { connectDB } = require("./config/database");
 const app = express();
-const User = require('./models/user');
+const User = require("./models/user");
 
 //middleware to convert json vaues readable
 app.use(express.json());
-app.post('/signup', async (req, res)=>{
-    //creating instance of user model
-    const user= new User(req.body);
+app.post("/signup", async (req, res) => {
+  //creating instance of user model
+  const user = new User(req.body);
 
-    try{
-       await user.save();
-       res.send('User registered successfully');  
-    }
-    catch(err){
-        res.status(400).send(err+' Invalid data');        
-    }
-})
+  try {
+    await user.save();
+    res.status(201).send("User registered successfully");
+  } catch (err) {
+    res.status(400).send(err + " Invalid data");
+  }
+});
 
-app.get('/user', async (req,res)=>{
-    try{
-        const user= await User.findOne({email:req.body.email});
-        if(!user){
-            return res.status(404).send('User not found');
-        }else{
-            res.send(user);
-        }}
-    catch(err){
-        res.status(400).send('Something went wrong');
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).send("User not found");
+    } else {
+      res.send(user);
     }
-})
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
 
-app.get('/user/:id',async(req,res)=>{
-    try{
-        const user= await User.findById(req.params.id);
-        if(!user){
-            res.status(404).send('User not found');
-        } else{
-            res.send(user);
-        }}
-    catch(err){
-        res.status(400).send('Something went wrong');
+app.get("/user/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
     }
-})
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
 
-app.get('/feed',async (req,res)=>{
-    try{
-        const users= await User.find();
-    if(users.length===0){
-        res.status(404).send('User not found');
-    }else{
-        res.send(users);
-    }}
-catch(err){
-    res.status(400).send('Something went wrong');
-}})
-
-app.delete('/user', async (req, res) => {   
-    try{
-        await User.findByIdAndDelete(req.body.id);
-        res.send('User deleted successfully');
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find();
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
     }
-    catch(err){
-        res.status(400).send('Something went wrong');
-    }
-})
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
 
-app.put('/user', async (req, res) => {
-    try{
-        const user= await User.findByIdAndUpdate(req.body.id,req.body,{returnDocument:"after"});
-        if(!user){
-            return res.status(404).send('User not found');
-        } else{
-            res.send(user);
-        }}
-    catch(err){
-        res.status(400).send('Something went wrong');
-    }
-})
+app.delete("/user", async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.body.id);
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
 
+app.put("/user/:id", async (req, res) => {
+  try {
+    const userId=req.params?.id;
+    const data= req.body;   
+
+    const ALLOWED_UPDATES =["photourl","skills","about","mobileNo","primaryAddress","permanentAddress"]
+    
+    const isallowed= Object.keys(data).every((k)=>ALLOWED_UPDATES.includes(k));
+
+    if(!isallowed){
+        throw new Error("Update not allowed");
+    }
+
+    if(data?.skills.length>20){ 
+        throw new Error("Cannot add more than 20 skills"); 
+    }
+
+    const user = await User.findByIdAndUpdate({_id:userId},data, {
+      returnDocument: "after",
+      runValidators: true
+    });
+    if (!user) {
+      return res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong: " + err.message);
+  }
+});
 
 connectDB()
-.then(()=>{
-    console.log('Successfully connected to db');
-    app.listen(7777,() => {
-        console.log('Server is running on port 7777');
+  .then(() => {
+    console.log("Successfully connected to db");
+    app.listen(7777, () => {
+      console.log("Server is running on port 7777");
     });
-})
-.catch((err)=>{
-    console.error('Error connecting to db:',err);
-})
+  })
+  .catch((err) => {
+    console.error("Error connecting to db:", err);
+  });
--- src/models/user.js
+++ src/models/user.js
@@ -1,32 +1,88 @@
-const mongoose= require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema= new mongoose.Schema({
-    firstName:{
-        type:String,
-        required:true
-    },
-    lastName:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    age:{
-        type:Number
-    },
-    isActive:{
-        type:Boolean,
-        default:true,
-        required:true
-    },
-    isDisabled:{
-        type:Boolean,
-        default:false,
-        required:true
-    }
+const addressSchema = new mongoose.Schema({
+  address: { type: String, maxLength: 30, trim: true },
+  street: { type: String, maxLength: 30, trim: true },
+  landmark: { type: String, maxLength: 30, trim: true },
+  city: { type: String, index: true, maxLength: 30, trim: true },
+  state: { type: String, maxLength: 30, trim: true },
+  country: { type: String, maxLength: 30, trim: true },
+  pincode: {
+    type: String,
+    index: true,
+    maxLength: 6,
+    minLength: 6,
+    trim: true,
+  },
 });
 
-module.exports=mongoose.model('User',userSchema);
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      maxLength: 50,
+      minLength: 2,
+      match: /^[a-zA-Z]+$/,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      maxLength: 50,
+      minLength: 2,
+      match: /^[a-zA-Z]+$/,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      trim: true,
+      lowercase: true,
+    },
+
+    age: {
+        type: Number,
+        min: 18,   
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+      required: true,
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+    skills: {
+      type: [String],
+      maxLength: 20,
+    },
+    about: {
+      type: String,
+      default: "this is me",
+    },
+    photourl: {
+      type: String,
+      default:
+        "https://static.vecteezy.com/system/resources/previews/008/442/086/non_2x/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg",
+    },
+    mobileNo: {
+      type: String,
+      maxLength: 10,
+      minLength: 8,
+      match: /^[0-9]$/,
+      unique: true,
+    },
+    primaryAddress: {
+      type: addressSchema,
+    },
+    permanentAddress: {
+      type: addressSchema,
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("User", userSchema);
