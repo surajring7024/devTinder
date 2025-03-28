f292ef4a80ee3dcf05f9c458e8635144a4b5034b
Suraj Ingole <suraj.ingole7024@gmail.com>
2025-03-28 23:09:36 +0530

    Added Cors for frontend

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@ const express = require("express");
 const { connectDB } = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
-
+const cors = require("cors");
 //middleware to convert json vaues readable
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  credentials: true, 
+}));
 
 const authRouter=require('./routes/authRouter');
 const profileRouter=require('./routes/profileRouter');
